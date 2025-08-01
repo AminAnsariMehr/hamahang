7c4da1e82a3ebefffcfbbf38a4979028d7ce6000
aminansarimehr <aminansarimehrgeek23@gmail.com>
2025-08-01 23:27:17 +0330

    goToOffice Shanbeh

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,38 @@
+<script setup>
+const icons = ['/src/', '/src/', '/src/', '/src/', '/src/']
+
+const menuItems = [
+  { id: '1', name: 'صفحه اصلی', route: '/home', icon: './src/assets/icons/menu/' },
+  {
+    id: '2',
+    name: 'دسته بندی',
+    route: '/categories',
+    icon: './src/assets/icons/menu/',
+  },
+  { id: '3', name: 'جستجو', route: '/search', icon: './src/assets/icons/menu/' },
+  {
+    id: '4',
+    name: 'لیست پخش',
+    route: '/watchlist',
+    icon: './src/assets/icons/menu/',
+  },
+  {
+    id: '5',
+    name: 'حساب کاربری',
+    route: '/profile',
+    icon: './src/assets/icons/menu/',
+  },
+]
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li v-for="menuItem in menuItems" :key="menuItem.id">
+        <router-link :to="menuItem.icon">{{ menuItem.name }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped></style>
